32199193d06084d252dce57714b7b89375cf415d
Michal Kozderka <kozderkamichal@gmail.com>
2021-10-26 09:18:10 +0200

    init
    Update README.md
    
    Update main.html
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+1. install meteor
+```
+npm install -g meteor
+```
+2. connect your android device to your pc
+3. insert google maps api key into client/main.html
+4. run
+```
+meteor npm install
+```
+5. run with your local ip address instead of 192.168.0.107
+```
+meteor run android-device --mobile-server http://192.168.0.107:3000
+```
---
+++ client/main.js
@@ -0,0 +1,52 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.onRendered(function helloOnCreated() {
+  // This example creates a simple polygon representing the Bermuda Triangle.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: { lat: 24.886, lng: -70.268 },
+    mapTypeId: "terrain",
+  });
+  // Define the LatLng coordinates for the polygon's path.
+  const triangleCoords = [
+    { lat: 25.774, lng: -80.19 },
+    { lat: 18.466, lng: -66.118 },
+    { lat: 32.321, lng: -64.757 },
+    { lat: 25.774, lng: -80.19 },
+  ];
+  // Construct the polygon.
+  const bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+
+  bermudaTriangle.setMap(map);
+}
+initMap()
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
